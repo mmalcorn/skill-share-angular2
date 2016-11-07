ca34875dc2067b795cb87dc7390496cd49db2552
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-11-07 16:32:32 -0500

    modify app component export class to reference both tables in firebase, and display the usernames in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,22 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
   template: `
   <h1> Testing for sharing! </h1>
 
-  <div class='skill' *ngFor='let skill of models | async'>
+  <div class='skill' *ngFor='let skill of skills | async'>
     <p>{{skill.skill_name}}</p>
   </div>
 
-
+  <div class='users' *ngFor='let user of users | async'>
+    <p>{{user.username}}</p>
+  </div>
   `
 
 })
 export class AppComponent {
-  models: FirebaseListObservable<any[]>;
+  skills: FirebaseListObservable<any[]>;
+  users: FirebaseListObservable<any[]>;
   constructor(angularFire: AngularFire){
-    this.models = angularFire.database.list('skills')
+    this.skills = angularFire.database.list('skills');
+    this.users = angularFire.database.list('users');
 
   }
 
