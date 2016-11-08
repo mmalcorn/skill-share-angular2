9a2f7fac9f9066180c985236bdcdfd9931257bc1
sp <sampeppard@gmail.com>
2016-11-08 14:39:58 -0500

    update router to include user-list component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'users',
+    component: UserListComponent
   },
 ];
 
