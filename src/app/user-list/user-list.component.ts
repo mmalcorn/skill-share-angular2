cff173ba0a2bc7114b452049d4dac2c4031ed942
sp <sampeppard@gmail.com>
2016-11-08 16:13:53 -0500

    add service to the user-list page to display users

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2';
+import { FirebaseDataService } from '../firebase-data.service';
 
 @Component({
   selector: 'app-user-list',
+  providers: [FirebaseDataService],
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public dataService: FirebaseDataService) { }
+  users: FirebaseListObservable<any[]>;
   ngOnInit() {
+    this.getDataFromService();
+  }
+  getDataFromService() {
+    this.users = this.dataService.getUsers();
   }
 
 }
