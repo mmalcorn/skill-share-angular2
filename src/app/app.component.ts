d5b32af076dc73fda96cc3149ac4745b44a49ec6
sp <sampeppard@gmail.com>
2016-11-08 16:20:28 -0500

    refactor app component to remove skills and user data calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
 import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
-import { FirebaseDataService } from './firebase-data.service';
-
 
 @Component({
   selector: 'app-root',
-  providers: [FirebaseDataService],
   template: `
   <router-outlet></router-outlet>
-
-  <h1> Testing for sharing! </h1>
-
-
-  <div class='users' *ngFor='let user of users | async'>
-    <p>{{user.username}}</p>
-  </div>
-  `
-
+`
 })
 export class AppComponent {
-  skills: FirebaseListObservable<any[]>;
-  users: FirebaseListObservable<any[]>;
-  constructor(angularFire: AngularFire, public dataService: FirebaseDataService){
-    // this.skills = angularFire.database.list('skills');
-    // this.users = angularFire.database.list('users');
-    this.getDataFromService();
-  }
-  getDataFromService() {
-    this.skills = this.dataService.getSkills();
-    this.users = this.dataService.getUsers();
-  }
-
 }
