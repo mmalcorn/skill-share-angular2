c21468f1204523307c181c5feb24b177d12b8175
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-11-11 13:50:42 -0500

    show users on skillspage

--- src/app/skill-details/skill-details.component.ts
+++ src/app/skill-details/skill-details.component.ts
@@ -3,6 +3,8 @@ import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { FirebaseDataService } from '../firebase-data.service';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-skill-details',
@@ -15,10 +17,13 @@ export class SkillDetailsComponent implements OnInit {
   constructor(
     public dataService: FirebaseDataService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private router: Router
   ) {}
   skills: FirebaseListObservable<any[]>;
   skill: FirebaseObjectObservable<any>;
+  users: FirebaseListObservable<any[]>;
+  userObjects = [];
   id: number;
 
   ngOnInit() {
@@ -27,10 +32,16 @@ export class SkillDetailsComponent implements OnInit {
   getDataFromService() {
     this.route.params.forEach((urlParametersArray) => {
       this.id = parseInt(urlParametersArray['id']);
-      console.log(this.id);
     });
-    // this.skills = this.dataService.getSkills();
+    this.users = this.dataService.getUsers();
     this.skill = this.dataService.goToSingleSkill(this.id);
+    // this.users.subscribe(function(snapshot) {
+    //   this.userObjects = snapshot;
+    // });
     console.log(this.skill);
+  }
+  goToUserDetail(userObject) {
+    console.log(userObject);
+    this.router.navigate(['users', userObject.$key]);
   }
 }
